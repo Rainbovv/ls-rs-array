dd41fff47efe19253a16dddd4378993764b71390
Radu Ţurcanu <63318406+Rainbovv@users.noreply.github.com>
2020-06-08 23:21:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,36 @@ public class Main {
         data = shiftLeftFor(data);
 
         for (Integer i: data) {
-                System.out.println(i);
+            System.out.println(i);
         }
-
     }
 
-/*  This method gets an array and moves the value of each cell(index) to cell(index+1)
+/*  This method gets an array and moves by iterations(from the last) the value of each cell[index-1] to cell[index]
 *   The first cell gets a null value.
 *   Returns changed array.  */
 
     public static Integer[] shiftRight(Integer[] array) {
         int i = array.length - 1;
+        
         while (i > 0) {
             array[i--] = array[i];
         }
+        
         array[i] = null;
         return array;
     }
 
-/*  This method gets an array and moves the value of each cell(index) to cell(index-1)
+/*  This method gets an array and moves by iterations(from first) the value of each cell[index+1] to cell[index]
 *   The last cell gets a null value.
 *   Returns changed array.  */
 
     public static Integer[] shiftLeft(Integer[] array) {
         int i = 0;
+        
         while (i < array.length - 1) {
             array[i++] = array[i];
         }
+        
         array[i] = null;
         return array;
     }
@@ -42,6 +45,7 @@ public class Main {
         for (int i = array.length - 1; i > 0; i--) {
             array[i] = array[i-1];
         }
+        
         array[0] = null;
         return array;
     }
@@ -51,6 +55,7 @@ public class Main {
         for (int i = 0; i < array.length - 1; i++) {
             array[i] = array[i+1];
         }
+        
         array[array.length - 1] = null;
         return array;
     }
